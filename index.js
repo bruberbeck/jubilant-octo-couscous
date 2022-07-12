00e50110d225a741dbd5f5fc6cc8a96f651775b4
Feyzi Çelik Yılankıran <fyilankiran@gmail.com>
2022-07-12 20:43:46 +0300

    Introduced graphiql

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 const app = express();
 const port = 3000;
+
+const schema = buildSchema(`
+	type Query {
+		hello: String
+	}
+`);
+
+const root = {
+	hello: () => {
+		return 'Hello world!';
+	}
+};
 
 app.get('/', (req, res) => {
 	res.send('Hello jubilant!');
 });
+
+app.use('/graphql', graphqlHTTP({
+	schema: schema,
+	rootValue: root,
+	graphiql: true
+}));
 
 app.listen(port, () => {
 	console.log(`Started listening on port ${port}`);
